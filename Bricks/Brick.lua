4b27a2cd13b086702fc898a7c1406cb1f58127af
Manuel Velarde <manuel.velarde@proton.me>
2024-05-11 09:26:06 -0600

    Fix overlap check

---
+++ Bricks/Brick.lua
@@ -0,0 +1,41 @@
+Brick = class{}
+
+-- Initialize brick object
+function Brick:init(others)
+    repeat
+        self.x = math.random(0, VIRTUAL_WIDTH - 30)
+        self.y = math.random(0, VIRTUAL_HEIGHT - 30)
+        self.width = math.random(10, 30)
+        self.height = math.random(10, 30)
+        self.left = self.x
+        self.right = self.x + self.width
+        self.top = self.y
+        self.bottom = self.y + self.height
+    until not intersects(self, others)
+    self.lifes = 3
+end
+
+-- Detect overlap
+function Brick:overlaps(other)
+    if self.left > other.right or other.left > self.right then
+        return false
+    end
+    if self.top > other.bottom or other.top > self.bottom then
+        return false
+    end
+    return true
+end
+
+-- Comparing over list
+function intersects(this, others)
+    for _, that in ipairs(others) do
+        if this:overlaps(that) then
+            return true
+        end
+    end
+    return false
+end
+
+function Brick:erode()
+    self.life = self.life - 1
+end
