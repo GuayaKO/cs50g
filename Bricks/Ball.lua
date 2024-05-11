0ad94cdfd8b2fbe293522320d310f705684e021c
Manuel Velarde <manuel.velarde@proton.me>
2024-05-11 09:11:05 -0600

    Remove deleted files

--- Bricks/Ball.lua
+++ Bricks/Ball.lua
@@ -11,7 +11,7 @@ function Ball:init(x, y, width, height)
 end
 
 -- Collision detection
-function Ball:collides(object)
+function Ball:collides(other)
 
     -- Calculate ball sides
     self.left = self.x
@@ -20,18 +20,18 @@ function Ball:collides(object)
     self.bottom = self.y + self.height
 
     -- Check for collision
-    if self.left > object.right or object.left > self.right then
+    if self.left > other.right or other.left > self.right then
         return nil
     end
-    if self.top > object.bottom or object.top > self.bottom then
+    if self.top > other.bottom or other.top > self.bottom then
         return nil
     end
     
     -- Find direction
-    left_overlap = math.abs(self.left - object.right)
-    right_overlap = math.abs(object.left - self.right)
-    top_overlap = math.abs(self.top - object.bottom)
-    bottom_overlap = math.abs(object.top - self.bottom)
+    left_overlap = math.abs(self.left - other.right)
+    right_overlap = math.abs(other.left - self.right)
+    top_overlap = math.abs(self.top - other.bottom)
+    bottom_overlap = math.abs(other.top - self.bottom)
     smallest_overlap = math.min(
         left_overlap,
         right_overlap,
@@ -52,10 +52,10 @@ function Ball:collides(object)
 end
 
 -- Calculate bounce
-function Ball:bounce(object, direction)
+function Ball:bounce(other, direction)
     if direction == "top" then
         self.dy = -ball.dy * 1.03
-        self.y = object.y + object.height
+        self.y = other.y + other.height
         if ball.dx < 0 then
             ball.dx = -math.random(10, 150)
         else
@@ -63,7 +63,7 @@ function Ball:bounce(object, direction)
         end
     elseif direction == "right" then
         self.dx = -ball.dx * 1.03
-        self.x = object.x - self.width
+        self.x = other.x - self.width
         if ball.dy < 0 then
             ball.dy = -math.random(10, 150)
         else
@@ -71,7 +71,7 @@ function Ball:bounce(object, direction)
         end
     elseif direction == "bottom" then
         self.dy = -ball.dy * 1.03
-        self.y = object.y - self.height
+        self.y = other.y - self.height
         if ball.dx < 0 then
             ball.dx = -math.random(10, 150)
         else
@@ -79,7 +79,7 @@ function Ball:bounce(object, direction)
         end
     else
         self.dx = -ball.dx * 1.03
-        self.x = object.x + object.width
+        self.x = other.x + other.width
         if ball.dy < 0 then
             ball.dy = -math.random(10, 150)
         else
